2860d9e2ece91d047801ce9a5488af2e0e9d8bfb
Taufikur <mukhamadtaufikurrachman@gmail.com>
2017-09-08 14:49:39 +0700

    Ngubah string tok

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class Trave extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Afik sayang jimmy!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
