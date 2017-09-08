8c1c65ea98348e25031c2986c290520d7d0dbbab
alfism1 <alfialfarisi@gmail.com>
2017-09-08 08:53:22 +0700

    C

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class Trave extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Master Chif!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
